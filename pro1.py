b3922dfd5e2c392559577988216948f4638c79e0
atishshingote <120120744+atishshingote@users.noreply.github.com>
2022-12-08 21:48:10 +0530

    Add files via upload

---
+++ pro1.py
@@ -0,0 +1,8 @@
+#here input
+old_age = input ("what is your age:")
+#here addition oh value of old age and 10
+new_age = int(old_age) + 10
+#here string of your result
+result = "now your age="
+#here print your result with its value
+print(result.upper() + str(new_age))
